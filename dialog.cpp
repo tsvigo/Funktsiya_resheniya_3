01df702d26d890c48714b7fd6dbbc08abd6c1903
Viktor Tsvigo <11_70@bk.ru>
2024-05-08 23:39:43 +0300

    Описание коммита

--- dialog.cpp
+++ dialog.cpp
@@ -3,13 +3,38 @@
 using namespace std;
 #include <iostream>
 #include <fstream>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QProcess>
+//########################################################################################################
+/// переменные:
+std::vector<unsigned long long> list_of_synapses ;
+std::vector<unsigned long long> list_of_neurons[201] ;//={};
+long long variable_error;
+QString  Nazvaniye_fayla_s_neyronami_i_signalom="";
+bool Odin_Uchitelia;
+bool Odin_Programmi;
+int var ;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//########################################################################################################
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Dialog)
 {
     ui->setupUi(this);
-    std::cout << "Funktsiya_Resheniya_3"<< std::endl;   
-}
+    std::cout << "Funktsiya_Resheniya_3"<< std::endl;  
+//########################################################################################################   
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// Открываем файл для решения
+
+   if (Nazvaniye_fayla_s_neyronami_i_signalom=="")
+   {
+Nazvaniye_fayla_s_neyronami_i_signalom = QFileDialog::getOpenFileName(this,
+              tr("Open txt"), "/home/viktor/my_projects_qt_2/Sgenerirovannye_fayly/peyzaji_2/", tr("Txt Files (*.txt)"));
+   }           
+              
+              
+              
+} // Dialog::Dialog(QWidget *parent)
 
 Dialog::~Dialog()
 {
